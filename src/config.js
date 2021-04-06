79695930fbf06a1210f17d2c2517122837950d3c
SophieCam <79154836+SophieCam@users.noreply.github.com>
2021-04-06 17:00:00 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+    authDomain: "react-firebase-chat-app-c0ca9.firebaseapp.com",
+    projectId: "react-firebase-chat-app-c0ca9",
+    storageBucket: "react-firebase-chat-app-c0ca9.appspot.com",
+    messagingSenderId: "95831760554",
+    appId: "1:95831760554:web:a30309402419a8b9b8fad3"
+};
